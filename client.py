cd57bcfd4c33cca68e36e8b1c5f918ca721a8c8e
Matthew Emerson <matthew.j.emerson@durham.ac.uk>
2025-11-02 06:40:56 +0000

    Changed win screen.

--- client.py
+++ client.py
@@ -273,9 +273,9 @@ def draw_board(screen, st: ClientState, board_img, x_img, o_img, z_img, font_sma
             winner_mark = macro_winner
             winner_name = st.player_names.get(winner_mark, "")
             if winner_name:
-                msg = f"{winner_name} ({winner_mark}) WINS!"
+                msg = f"{winner_name.toUpper()} WINS!"
             else:
-                msg = f"{winner_mark} WINS!"
+                msg = f"{winner_mark.toUpper()} WINS!"
             colour = (239, 246, 255)
         else:
             msg = "DRAW!"
